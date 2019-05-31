49333a3b57a20d57c981592f980bc77a7164b448
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-31 11:53:46 +0100

    create SSH public key

--- experimental/k8s/scripts/cluster_config.sh
+++ experimental/k8s/scripts/cluster_config.sh
@@ -26,6 +26,7 @@ export kops_bucket_name=mbdebian-k8s-experimental-state-store
 export KOPS_STATE_STORE=s3://mbdebian-k8s-experimental-state-store
 export cluster_zones="eu-north-1a"
 export s3_bucket_region="eu-north-1"
+export cluster_public_key_file="~/.ssh/bofh_aws_terraform.pub"
 
 # Show information
 loginfo "Cluster configuration for"
@@ -42,7 +43,7 @@ logdevops "Enabling versioning on '$KOPS_STATE_STORE'"
 aws s3api put-bucket-versioning --bucket $kops_bucket_name  --versioning-configuration Status=Enabled
 
 # Create cluster SSH public key
-# Not yet
+$KOPS create secret --name $cluster_name sshpublickey admin -i $cluster_public_key_file
 
 # Create cluster configuration
 $KOPS create cluster --name=$cluster_name \

5ec892eade95c3439505a2917f72e89f25b8f4e3
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-18 11:25:05 +0100

    updated certificate request for labs

--- experimental/idorg-labs/request_certificates.sh
+++ experimental/idorg-labs/request_certificates.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcloud beta compute ssl-certificates create idorg-labs-satellite-api-resolver --domains resolver.api.labs.identifiers.org
 gcloud beta compute ssl-certificates create idorg-labs-satellite-api-metadata --domains metadata.api.labs.identifiers.org
-gcloud beta compute ssl-certificates create idorg-labs-satellite-web --domains cloud.labs.identifiers.org
+gcloud beta compute ssl-certificates create idorg-labs-satellite-web --domains labs.identifiers.org
